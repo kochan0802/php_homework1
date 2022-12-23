e0d24eab71cada630f277f15f3316d6e6a14fee6
kochan0802 <115932327+kochan0802@users.noreply.github.com>
2022-12-23 11:41:30 +1000

    update

--- classes/UserLogic.php
+++ classes/UserLogic.php
@@ -11,6 +11,7 @@ class UserLogic
      *  
      * boolとはtrueとfalseを表す
      */ 
+
     public static function createUser($userData)   
     {
         $result = false;
@@ -33,5 +34,101 @@ class UserLogic
         } catch(\Exception $e) {      //$eはExceptionを受け取る任意の変数
           return $result;
         }
+      }
+  
+    /**
+     *ログイン処理
+     * @param string $email
+     * @param string $password
+     * @return bool $result
+    
+     */ 
+    public static function login($email, $password)   
+    {
+      //結果
+      $result = false;
+      // ユーザーをemailから検索して取得
+      $user = self::getUserByEmail($email);
+      //  var_dump($user);
+      // return;    
+
+      if(!$user){
+        $_SESSION['msg'] = 'emailが一致しません。';
+        return $result;
+      } 
+      
+        
+
+
+  // パスワードの照会
+  if (password_verify($password, $user['password'])) {
+    //ログイン成功
+    //古いセッションを破棄して、新しいセッションを作り直す（セッションハイジャック対策）
+    session_regenerate_id(true);
+    $_SESSION['login_user'] = $user;
+    $result = true;
+    return $result;
+  }
+  
+    $_SESSION['msg'] = 'パスワードが一致しません。';
+    return $result;
+  }
+    /**
+     *emailからユーザーを取得
+     * @param string $email
+     * @return array |bool $user|false
+     */ 
+    public static function getUserByEmail ($email)   
+    {
+      // SQLの準備
+      // SQLの実行 
+      // SQLの結果を返す
+       $sql = 'SELECT * FROM users WHERE email = ?';
+
+        //emailを配列に入れる
+        $arr = [];  
+        $arr[] = $email;
+   
+        //例外処理を入れる 
+        try { 
+          $stmt = connect()->prepare($sql);    
+          $stmt->execute($arr);
+          // SQLの結果を返す
+          $user = $stmt->fetch();
+          return $user;            
+        } catch(\Exception $e) {     
+          return $false;
+        }
+  
+      }
+    /**
+      *ログインチェック
+      * @param void
+      * @return bool $result
+      */ 
+      public static function checkLogin ()   
+      {
+
+        $result = false;
+   
+
+        //セッションにログインユーザーが入っていなかったらfalse
+        if (isset($_SESSION['login_user'])&& $_SESSION['login_user']['id'] >
+         0){
+          return $result = true;
+         }
+
+         return $result;
+ } 
+
+    /**
+     * ログアウト処理
+     */
+
+    public static function logout()
+    {
+      $_SESSION = array(); //セッションの中身を空にする
+      session_destroy(); //セッションを消す
     }
+
 }
